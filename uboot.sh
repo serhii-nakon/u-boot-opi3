815994d72a1c3994f89386673f40b19649f69378
serhii.n <serhii.n@thescimus.com>
2022-07-18 23:59:09 +0300

    Update U-Boot v2022.07, ATF v2.7, SCP (CRUST) v0.5

--- uboot.sh
+++ uboot.sh
@@ -6,18 +6,18 @@ apt update
 apt install gcc-aarch64-linux-gnu bison flex swig python2-dev build-essential device-tree-compiler python3 python3-dev libssl-dev git bc python3-setuptools sed gcc-or1k-elf -y
 
 cd /root
-git clone --single-branch --branch v2022.04 https://github.com/u-boot/u-boot.git
-git clone --single-branch --branch v2.6 https://github.com/ARM-software/arm-trusted-firmware.git
+git clone --single-branch --branch v2022.07 https://github.com/u-boot/u-boot.git
+git clone --single-branch --branch v2.7 https://github.com/ARM-software/arm-trusted-firmware.git
 git clone --single-branch --branch v0.5 https://github.com/crust-firmware/crust.git
 
 cd /root/arm-trusted-firmware
-
-patch -p1 < /root/patches/0001-allow_to_skip_PMIC_regulator_setup.patch
+export LDFLAGS="--no-warn-rwx-segments"
 make -j 16 CROSS_COMPILE=aarch64-linux-gnu- PLAT=sun50i_h6 SUNXI_SETUP_REGULATORS=0 bl31
 
 cd /root/crust
-
-make -j 16 CROSS_COMPILE=aarch64-linux-gnu- orangepi_3_defconfig
+patch -p1 < /root/patches/0001-add_ability_set_ldflags.patch
+export LDFLAGS="-Wl,--no-warn-rwx-segments"
+make -j 16 CROSS_COMPILE=or1k-elf- orangepi_3_defconfig
 make -j 16 CROSS_COMPILE=or1k-elf- scp
 
 cd /root/u-boot
